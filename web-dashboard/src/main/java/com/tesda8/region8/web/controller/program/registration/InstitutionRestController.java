b81abb6483d02de31f4127d82ac5e1890c64940f
dvlbamasa <dvlbamasa@gmail.com>
2020-10-27 09:12:04 +0800

    fixed bug on data table

--- web-dashboard/src/main/java/com/tesda8/region8/web/controller/program/registration/InstitutionRestController.java
+++ web-dashboard/src/main/java/com/tesda8/region8/web/controller/program/registration/InstitutionRestController.java
@@ -2,7 +2,9 @@ package com.tesda8.region8.web.controller.program.registration;
 
 import com.tesda8.region8.program.registration.model.dto.InstitutionDto;
 import com.tesda8.region8.program.registration.service.InstitutionService;
+import com.tesda8.region8.util.enums.Sector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +25,10 @@ public class InstitutionRestController {
     @RequestMapping(method = RequestMethod.GET, value = "/all")
     public List<InstitutionDto> getAllInstitutions() {
         return institutionService.getAllInstitution();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{sector}/sector")
+    public List<InstitutionDto> getAllInstitutionByCourseSector(@PathVariable("sector")Sector sector) {
+        return institutionService.getAllInstitutionByCourseSector(sector);
     }
 }
