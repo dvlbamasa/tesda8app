3537d921e661a42f74da0777bc3bd0f3ad2aeed5
dvlbamasa <dvlbamasa@gmail.com>
2020-11-18 11:48:58 +0800

    implemented download OPCR graphs

--- web-dashboard/src/main/resources/static/js/graph_opcr.js
+++ web-dashboard/src/main/resources/static/js/graph_opcr.js
@@ -97,3 +97,47 @@ function generateGraph(urlTarget, chartName, title, legend1, legend2) {
         chart.render();
     });
 }
+
+
+$("#downloadGraphButton").click(function(){
+    var HTML_Width = $(".canvas_div_pdf").width();
+    var HTML_Height = $(".canvas_div_pdf").height();
+    var top_left_margin = 15;
+    var PDF_Width = HTML_Width+(top_left_margin*2);
+    var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
+    var canvas_image_width = HTML_Width;
+    var canvas_image_height = HTML_Height;
+
+    var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
+
+    var papGroupTypeLabel = $('#papGroupTypeLabel').val();
+    setTimeout(function () {
+        html2canvas($(".canvas_div_pdf")[0],{allowTaint:true}).then(function(canvas) {
+            canvas.getContext('2d');
+
+            console.log(canvas.height+"  "+canvas.width);
+
+
+            var imgData = canvas.toDataURL("image/jpeg", 1.0);
+            var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
+            pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
+
+
+            for (var i = 1; i <= totalPDFPages; i++) {
+                var offset;
+                if (i === 3) {
+                    offset = 60;
+                } else if (i > 3) {
+                    offset += 30;
+                } else {
+                    offset = 0;
+                }
+                pdf.addPage(PDF_Width, PDF_Height);
+                pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4) + offset,canvas_image_width,canvas_image_height);
+            }
+
+            pdf.save("TESDA-" + papGroupTypeLabel + " Graphs.pdf");
+        });
+    }, 2000);
+
+});
