d7cf089587ac1dfda1f3f95cfe722f5bc63bd2d0
dvlbamasa <dvlbamasa@gmail.com>
2020-11-19 13:23:29 +0800

    fixed performance issues

--- util/src/main/java/com/tesda8/region8/util/enums/OperatingUnitPOType.java
+++ util/src/main/java/com/tesda8/region8/util/enums/OperatingUnitPOType.java
@@ -2,19 +2,22 @@ package com.tesda8.region8.util.enums;
 
 public enum OperatingUnitPOType {
 
-    LEYTE_PO("Leyte PO"),
-    BILIRAN_PO("Biliran PO"),
-    SOUTHERN_LEYTE_PO("Southern Leyte PO"),
-    SAMAR_PO("Samar PO"),
-    EASTERN_SAMAR_PO("Eastern Samar PO"),
-    NORTHERN_SAMAR_PO("Northern Samar PO"),
-    TESDA_RO("TESDA RO"),
+    LEYTE_PO("Leyte PO", "HIGHLIGHTED"),
+    BILIRAN_PO("Biliran PO", "NON-HIGHLIGHTED"),
+    SOUTHERN_LEYTE_PO("Southern Leyte PO", "HIGHLIGHTED"),
+    SAMAR_PO("Samar PO", "NON-HIGHLIGHTED"),
+    EASTERN_SAMAR_PO("Eastern Samar PO", "HIGHLIGHTED"),
+    NORTHERN_SAMAR_PO("Northern Samar PO", "NON-HIGHLIGHTED"),
+    TESDA_RO("TESDA RO", "HIGHLIGHTED"),
 
-    TOTAL("Region 8");
+    TOTAL("Region 8", "NON-HIGHLIGHTED");
 
     public final String label;
+    public final String tableType;
 
-    private OperatingUnitPOType(String label) {
+
+    private OperatingUnitPOType(String label, String tableType) {
         this.label = label;
+        this.tableType = tableType;
     }
 }
