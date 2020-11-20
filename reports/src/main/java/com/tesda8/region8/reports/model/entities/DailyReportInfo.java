1bd92ce8d97751998d7a7b89e4577063a926a833
dvlbamasa <dvlbamasa@gmail.com>
2020-11-20 08:41:10 +0800

    fixed id error

--- reports/src/main/java/com/tesda8/region8/reports/model/entities/DailyReportInfo.java
+++ reports/src/main/java/com/tesda8/region8/reports/model/entities/DailyReportInfo.java
@@ -1,12 +1,12 @@
 package com.tesda8.region8.reports.model.entities;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.time.LocalDateTime;
 
